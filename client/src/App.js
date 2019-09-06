6b80119c5b9231db61849075710fe1b7f4660ad3
Brandon Dong <gasingdong@gmail.com>
2019-09-06 10:23:33 -0700

    render projects list from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/projects');
+        setProjects(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, [])
+
   return (
     <div className="App">
+      {
+        projects.map(project => (
+          <Fragment key={project.id}>
+            <h1>{project.name}</h1>
+            <p>{project.description}</p>
+          </Fragment>
+        ))
+      }
     </div>
   );
 }
