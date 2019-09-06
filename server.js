a39a86a43af212e55c3bcc67f6ff486f34975131
Brandon Dong <gasingdong@gmail.com>
2019-09-06 10:22:53 -0700

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const apiRouter = require('./api/apiRouter');
 
 const server = express();
 
+server.use(cors());
 server.use('/api', apiRouter);
 
 module.exports = server;
